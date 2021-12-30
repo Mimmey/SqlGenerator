7114c14a99162b0299a7e1127f5c266362c0da46
Mimmey <motyolya@gmail.com>
2021-12-30 19:36:18 +0300

    Fixed emptiness

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,7 @@ package com.company;
 import java.io.*;
 
 public class Main {
-
-
+    
     public static void main(String[] args) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("dmlScript.txt"))) {
             Generator generator = new Generator(writer);
